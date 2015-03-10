4b90bf546c6d9c98bbb48bc8ffbb55ac61260731
Tobias Hagemann <tobias@hagemann.org>
2015-03-10 17:03:51 +0100

    Initial Commit

---
+++ sissy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sissy
+//
+//  Created by Tobias Hagemann on 10/03/15.
+//  Copyright (c) 2015 tobiha.de. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
